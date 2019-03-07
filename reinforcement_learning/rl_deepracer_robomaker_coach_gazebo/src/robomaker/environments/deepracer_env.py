a4a7d5dbb0b634e5ef758e6e4f4d5813f59ac0bf
PatrickBuhagiar <patrickbhgr@outlook.com>
2019-03-07 22:17:32 +0000

    simplified reward function

--- reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/robomaker/environments/deepracer_env.py
+++ reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/robomaker/environments/deepracer_env.py
@@ -187,11 +187,11 @@ class DeepRacerEnv(gym.Env):
 
     def reward_function(self, on_track, x, y, distance_from_center, car_orientation, progress, steps,
                         throttle, steering, track_width, waypoints, closest_waypoints):
-        if distance_from_center >= 0.0 and distance_from_center <= 0.02:
+        if 0.0 <= distance_from_center <= 0.02:
             return 1.0
-        elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
+        elif 0.02 <= distance_from_center <= 0.03:
             return 0.3
-        elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
+        elif 0.03 <= distance_from_center <= 0.05:
             return 0.1
         return 1e-3  # like crashed
 
